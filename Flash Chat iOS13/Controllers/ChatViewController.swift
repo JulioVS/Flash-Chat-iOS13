3cb53c12285dbe4fd9c017db625029e3d943b4b5
JulioVS <julioer@me.com>
2026-01-24 19:19:50 -0300

    Add User Logout

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ChatViewController: UIViewController {
+//  Add Firebase libraries
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
 
+class ChatViewController: UIViewController {
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
+        
+        title = "⚡️FlashChat"
+        navigationItem.hidesBackButton = true
+        
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        
     }
     
-
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+                
+        do {
+            
+            try Auth.auth().signOut()
+            print("Successful Logout")
+            
+            navigationController?.popToRootViewController(animated: true)
+            
+        }
+        catch let signOutError as NSError {
+            
+            print("Error signing out: %@", signOutError)
+            
+        }
+        
+    }
+    
 }
